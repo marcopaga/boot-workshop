b119d504d2bb2368ab20dfdfe2f44e33c43bb568
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2017-11-21 00:19:08 +0100

    made URI for trucks service configurable.

--- locations/src/main/java/de/codecentric/boot/workshop/controller/Locations.java
+++ locations/src/main/java/de/codecentric/boot/workshop/controller/Locations.java
@@ -1,5 +1,6 @@
 package de.codecentric.boot.workshop.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,14 +17,17 @@ public class Locations {
 
     private RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${service.trucks.uri}")
+    private String trucksServiceUri;
+
     @RequestMapping(method = RequestMethod.GET, path = "/")
-    public ResponseEntity<Iterable<String>> list(){
+    public ResponseEntity<Iterable<String>> list() {
         return new ResponseEntity<Iterable<String>>(Arrays.asList("Köln", "Erkrath", "Düsseldorf"), HttpStatus.OK);
     }
 
     @RequestMapping(path = "/{location}", method = RequestMethod.GET)
     public String location(@PathVariable("location") final String location) {
-        return restTemplate.getForObject("http://localhost:8080/trucks/", String.class);
+        return restTemplate.getForObject(trucksServiceUri, String.class);
     }
 
 }
