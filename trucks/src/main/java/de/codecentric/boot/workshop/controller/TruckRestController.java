9f0f3012ceff234405bd8cdefa0ecf599be9601f
Yevgeniy Melnichuk <yevgeniy.melnichuk@googlemail.com>
2017-11-22 22:51:29 +0100

    added a pageable query to TruckRepository

--- trucks/src/main/java/de/codecentric/boot/workshop/controller/TruckRestController.java
+++ trucks/src/main/java/de/codecentric/boot/workshop/controller/TruckRestController.java
@@ -3,11 +3,9 @@ package de.codecentric.boot.workshop.controller;
 import de.codecentric.boot.workshop.model.Truck;
 import de.codecentric.boot.workshop.model.TruckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -21,6 +19,11 @@ public class TruckRestController {
     @RequestMapping(method = RequestMethod.GET, path = "/trucks")
     public Iterable<Truck> allTrucks(){
         return truckRepository.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/trucks/{location}")
+    public Iterable<Truck> trucksByLocation(@PathVariable("location") final String location){
+        return truckRepository.findByLocation(location, new PageRequest(0, 2));
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/truck")
--- trucks/src/main/java/de/codecentric/boot/workshop/model/TruckRepository.java
+++ trucks/src/main/java/de/codecentric/boot/workshop/model/TruckRepository.java
@@ -1,8 +1,14 @@
 package de.codecentric.boot.workshop.model;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface TruckRepository extends CrudRepository<Truck, Long> {
+    Page<Truck> findByLocation(@Param("location") String location, Pageable p);
 }
